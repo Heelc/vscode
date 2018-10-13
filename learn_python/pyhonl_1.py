3207d1234af5994486d73b6e7bebe90d72f8f055
Heelc <heelc@outlook.com>
2018-10-13 19:19:51 +0800

    修改了note1
    查看修改

--- learn_python/pyhonl_1.py
+++ learn_python/pyhonl_1.py
@@ -1,22 +1,8 @@
-l1 = [1,2,3]
-l1.append(4)
-def test1(*l):
-    l2 = []
-    for i in l:
-        l2.append(i)
-    l2.append(5)        
-    return l2
-
-# l2 = test1(*l1)
-
-# print(l1)
-# print(l2)
-
-def test2(arg1,arg2):
-    print(arg1)
-    pass
-
-test2(1,2)
-
-print("wai ? Fei yuan zai ma?")
-
+n = 100
+a = 1
+b = 1
+while n > 0:
+    print(b)
+    a,b = b,a+b
+    n = n - 1
+note1 = "v3"
